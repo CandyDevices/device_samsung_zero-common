9a9b8c29982f1a6be24af22fa05ec959d308b574
Lukas0610 <mail@lukasberger.at>
2018-03-12 15:50:12 +0100

    zero: reconfigure dual-SIM-support to single tree again
    To reduce amount of builds and build duration, previous changes already introduced lot of changes
    to achieve supports several variants in a single build based on zerofltexx.
    
    This continues these changes, removes seperated single/dual-SIM configurations, and completley
    erases the manfiest-entries for radio to prevent bootloop caused by non-existing slot2 on single
    SIM devices.

--- ramdisk/Android.mk
+++ ramdisk/Android.mk
@@ -61,6 +61,14 @@ include $(BUILD_PREBUILT)
 endif
 
 include $(CLEAR_VARS)
+LOCAL_MODULE            := init.rilchip.sh
+LOCAL_MODULE_TAGS       := optional eng
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_SRC_FILES         := init.rilchip.sh
+LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE            := init.wifi.rc
 LOCAL_MODULE_TAGS       := optional eng
 LOCAL_MODULE_CLASS      := ETC
